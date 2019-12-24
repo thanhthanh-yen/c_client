5300c5ab5e3e64ba0826f940a1b59d7ea8ad1850
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-24 15:03:07 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -7,11 +7,12 @@ wget https://github.com/griddb/griddb_nosql/releases/download/v4.3.0/griddb-nosq
 sudo dpkg -i griddb-nosql_4.3_amd64.deb
 
 # Start server
-[gsadm]$ gs_passwd admin
-[gsadm]$ sed -i 's/"clusterName":""/"clusterName":"griddbcentos"/g' conf/gs_cluster.json
-[gsadm]$ export no_proxy=127.0.0.1
-[gsadm]$ gs_startnode -u admin/admin -w
-[gsadm]$ gs_joincluster -c griddbcentos -u admin/admin
+su gsadm -l
+gs_passwd admin
+sed -i 's/"clusterName":""/"clusterName":"griddbcentos"/g' conf/gs_cluster.json
+export no_proxy=127.0.0.1
+gs_startnode -u admin/admin -w
+gs_joincluster -c griddbcentos -u admin/admin
 
 # run sample
 cp client/c/sample/sample1.c .
