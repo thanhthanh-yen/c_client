ba2bf36452af657d1668dbf0c6c2c6b70c90734b
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-25 10:21:43 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -8,7 +8,7 @@ export GS_HOME=/var/lib/gridstore
 export GS_LOG=/var/lib/gridstore/log
 
 # Start server
-sudo su gsadm -l
+sudo su gsadm -l && gs_passwd admin -p admin
 gs_passwd admin -p admin
 sed -i 's/"clusterName":""/"clusterName":"griddbcentos"/g' $GS_HOME/conf/gs_cluster.json
 export no_proxy=127.0.0.1
