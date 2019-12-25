e4bfcd7de07d367c6605714e270876c6c8ee781b
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-25 18:10:52 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -6,11 +6,11 @@ sudo dpkg -i griddb-nosql_4.3_amd64.deb
 
 # Start server
 sudo -l -u gsadm bash -c "gs_passwd admin -p admin"
-sudo -l -u gsadm bash -c "sed -i 's/"clusterName":""/"clusterName":"griddbcentos"/g' conf/gs_cluster.json"
+sudo -l -u gsadm bash -c "sed -i 's/"clusterName":""/"clusterName":"griddbubuntu"/g' conf/gs_cluster.json"
 export no_proxy=127.0.0.1
 sudo -l -u gsadm bash -c "gs_startnode -u admin/admin -w"
 sleep 5
-sudo -l -u gsadm bash -c "gs_joincluster -c griddbcentos -u admin/admin"
+sudo -l -u gsadm bash -c "gs_joincluster -c griddbubuntu -u admin/admin"
 
 # run sample
 cp client/c/sample/sample1.c .
@@ -19,5 +19,5 @@ gcc-4.8 -I./client/c/include -L./bin sample1.c -l gridstore
 ls
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./bin
 sleep 5
-./a.out 239.0.0.1 31999 griddbcentos admin admin
+./a.out 239.0.0.1 31999 griddbubuntu admin admin
 echo "here"
