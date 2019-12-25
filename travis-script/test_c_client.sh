a659780cdc9338f81828c1b8c01c083edb08119b
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-25 14:45:53 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -9,15 +9,15 @@ sudo -l -u gsadm bash -c "gs_passwd admin -p admin"
 sudo -l -u gsadm bash -c "sed -i 's/"clusterName":""/"clusterName":"griddbcentos"/g' conf/gs_cluster.json"
 export no_proxy=127.0.0.1
 sudo -l -u gsadm bash -c "gs_startnode -u admin/admin -w"
-sleep 10
+sleep 5
 sudo -l -u gsadm bash -c "gs_joincluster -c griddbcentos -u admin/admin"
-sleep 10
 
 # run sample
 cp client/c/sample/sample1.c .
 gcc --version
-gcc-4.8 -I./client/c/include -L./bin sample1.c -lgridstore
+gcc-4.8 -I./client/c/include -L./bin sample1.c -l gridstore
 ls
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./bin
+sleep 5
 ./a.out 239.0.0.1 31999 griddbcentos admin admin
 echo "here"
