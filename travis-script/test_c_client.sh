595670c4adaca3072916b619bb6978dffe281a10
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-26 10:56:55 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -8,8 +8,9 @@ sudo dpkg -i griddb-nosql_4.3_amd64.deb
 sudo su - gsadm -c "gs_passwd admin -p admin"
 sudo su - gsadm -c "sed -i 's/"clusterName":""/"clusterName":"griddbubuntu"/g' conf/gs_cluster.json"
 sudo su - gsadm -c "export no_proxy=127.0.0.1"
-sudo -l su - gsadm -c "gs_startnode -u admin/admin -w"
-sudo -l su - gsadm -c "gs_joincluster -c griddbubuntu -u admin/admin"
+sudo su - gsadm -c "gs_startnode -u admin/admin -w"
+sudo su - gsadm -c "sleep 5"
+sudo su - gsadm -c "gs_joincluster -c griddbubuntu -u admin/admin"
 
 # run sample
 cp client/c/sample/sample1.c .
