02c828c446184689f002901893d748403bd4cb2f
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2019-12-27 14:07:52 +0700

    Update start_server.sh

--- travis-script/start_server.sh
+++ travis-script/start_server.sh
@@ -8,6 +8,6 @@ sudo su - gsadm -c "gs_passwd admin -p admin"
 sudo su - gsadm -c "sed -i 's/\"clusterName\":\"\"/\"clusterName\":\"griddbubuntu\"/g' conf/gs_cluster.json"
 sudo su - gsadm -c "export no_proxy=127.0.0.1"
 sudo su - gsadm -c "gs_startnode -u admin/admin -w"
-sudo su - gsadm -c "gs_joincluster -c griddbubuntu -u admin/admin"
+sudo su - gsadm -c "gs_joincluster -c griddbubuntu -u admin/admi"
 
 
