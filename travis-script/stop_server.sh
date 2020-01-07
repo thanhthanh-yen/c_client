3d55750f5faad54763a35ae97925d98cc4bebac0
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-07 11:00:22 +0700

    Update stop_server.sh

--- travis-script/stop_server.sh
+++ travis-script/stop_server.sh
@@ -3,5 +3,5 @@
 export GS_HOME=$PWD
 export GS_LOG=$PWD/log
 
-sudo su - gsadm -c "bin/gs_stopcluster -u admin/admin"
-sudo su - gsadm -c "bin/gs_stopnode -u admin/admin"
+sudo su - gsadm -c "gs_stopcluster -u admin/admin"
+sudo su - gsadm -c "gs_stopnode -u admin/admin"
