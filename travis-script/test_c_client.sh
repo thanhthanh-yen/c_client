370b001974b82a1b368e52bf1d8b796d7595b99f
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:08:59 +0700

    Update test_c_client.sh

--- travis-script/test_c_client.sh
+++ travis-script/test_c_client.sh
@@ -3,4 +3,5 @@
 cp client/c/sample/sample1.c .
 gcc-4.8 -I./client/c/include -L./bin sample1.c -l gridstore
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./bin
+ls
 ./a.out 239.0.0.1 31999 griddbubuntu admin admin
