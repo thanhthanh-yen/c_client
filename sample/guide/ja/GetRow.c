95b67e32f4c71864e56b4b8be576a907b18b84b9
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:49:47 +0700

    Update GetRow.c

--- sample/guide/ja/GetRow.c
+++ sample/guide/ja/GetRow.c
@@ -52,7 +52,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};		
 
 	/*
