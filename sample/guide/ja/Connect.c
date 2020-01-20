cab1b04666d42e90a01cdb2463063b7e4a6e2eee
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:48:38 +0700

    Update Connect.c

--- sample/guide/ja/Connect.c
+++ sample/guide/ja/Connect.c
@@ -23,7 +23,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};
 
 	/*
