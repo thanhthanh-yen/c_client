f566492e3cd3c19bf7689c044253032377734f1e
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:50:39 +0700

    Update TQLAggregation.c

--- sample/guide/ja/TQLAggregation.c
+++ sample/guide/ja/TQLAggregation.c
@@ -58,7 +58,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};		
 
 	/*
