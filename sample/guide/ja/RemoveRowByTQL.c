a493da79739440939f91c61f027e26d2a92931d6
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:50:28 +0700

    Update RemoveRowByTQL.c

--- sample/guide/ja/RemoveRowByTQL.c
+++ sample/guide/ja/RemoveRowByTQL.c
@@ -53,7 +53,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};		
 
 	/*
