13b6be47a399af7555fff4cdb8b158d3e7560c4c
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:26:01 +0700

    update server

--- travis-script/start_server.sh
+++ travis-script/start_server.sh
@@ -5,9 +5,9 @@ sudo dpkg -i griddb-nosql_4.3_amd64.deb
 
 # Start server
 sudo su - gsadm -c "gs_passwd admin -p admin"
-sudo su - gsadm -c "sed -i 's/\"clusterName\":\"\"/\"clusterName\":\"griddbubuntu\"/g' conf/gs_cluster.json"
+sudo su - gsadm -c "sed -i 's/\"clusterName\":\"\"/\"clusterName\":\"myCluster\"/g' conf/gs_cluster.json"
 sudo su - gsadm -c "export no_proxy=127.0.0.1"
 sudo su - gsadm -c "gs_startnode -u admin/admin -w"
-sudo su - gsadm -c "gs_joincluster -c griddbubuntu -u admin/admin"
+sudo su - gsadm -c "gs_joincluster -c myCluster -u admin/admin"
 
 
