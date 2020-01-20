9b73535a5d0125cbeb730deb1db06faa50e6a956
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:39:20 +0700

    Update ArrayData.c

--- sample/guide/ja/ArrayData.c
+++ sample/guide/ja/ArrayData.c
@@ -29,7 +29,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};		
 
 	/*
