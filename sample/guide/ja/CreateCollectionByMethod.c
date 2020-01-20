446fdeb9974bf15f7b8ee26f9528cb4312ceeb9e
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-01-20 14:49:28 +0700

    Update CreateCollectionByMethod.c

--- sample/guide/ja/CreateCollectionByMethod.c
+++ sample/guide/ja/CreateCollectionByMethod.c
@@ -27,7 +27,7 @@ void main(int argc, char *argv[]){
 		{ "database", "public" },
 		{ "user", "admin" },
 		{ "password", "admin" },
-		{ "applicationName", "SampleC" }
+		//{ "applicationName", "SampleC" }
 	};		
 
 	/*
